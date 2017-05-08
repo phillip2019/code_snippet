2f5478c6b33519a98425011399a0d8e3d63d21f7
xiaowei.song <xiaowei.song@tongdun.cn>
2017-05-08 15:07:29 +0800

    pep8 format

--- python/zombie_process.py
+++ python/zombie_process.py
@@ -9,24 +9,22 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import os
-import sys
 import time
-import traceback
-
 
 def main():
-	pid = os.fork()
-	if 0 > pid:
-		print('fork err:')  # fork failed
-	elif 0 == pid:
-		print('I am in child process, and child exit!')  # child process
-		exit(0)
-	else:
-		print('I am in parent process, sleep for 1 minute...ZZ...')  # parent process
-		time.sleep(60)
-		print('I am in process, and parent exit!')
-		exit(0)
+    pid = os.fork()
+    if 0 > pid:
+        print('fork err:')  # fork failed
+    elif 0 == pid:
+        print('I am in child process, and child exit!')  # child process
+        exit(0)
+    else:
+        print('I am in parent process, sleep for '
+        	  ' 1 minute...ZZ...')  # parent process
+        time.sleep(60)
+        print('I am in process, and parent exit!')
+        exit(0)
 
 
 if __name__ == '__main__':
-	main()
+    main()
