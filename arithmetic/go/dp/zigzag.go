78613eb0ac62b1cc5b3f2dc0bdd9c8d9e71260f3
Shane <msn734506700@Live.com>
2018-12-02 12:12:42 +0800

    change

--- arithmetic/go/dp/zigzag.go
+++ arithmetic/go/dp/zigzag.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	//Enter your code here. Read input from STDIN. Print output to STDOUT、
+	var N, z int
+	var A, d []int
+	fmt.Scanln(&N)
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	inputs := strings.Split(input, " ")
+	for i, s := range inputs {
+		if i == N-1 {
+			s = strings.Replace(s, "\n", "", -1)
+		}
+		a, _ := strconv.Atoi(s)
+		A = append(A, a)
+	}
+
+	for i := 0; i < N; i++ {
+		d = append(d, 1)
+	}
+
+	for i := 1; i < N; i++ {
+		if A[i-1] < A[i] {
+			d[i] = d[i-1] + 1
+		} else if d[i-1] >= 3 {
+			z += d[i-1] - 2
+		}
+	}
+	fmt.Println(z)
+}
--- arithmetic/go/str/common.go
+++ arithmetic/go/str/common.go
@@ -1,1 +1,73 @@
-package str
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+// 字符串：倒置算法
+func reverse(s []string) {
+	for i, j := 0, len(s) - 1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
+
+// 字符串：倒置算法，数组指针法
+func reverseArray(s *[9]string) {
+	for i, j := 0, len(s) - 1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
+
+
+// 比较slice是否相等
+func equalSlice(x, y []string) bool {
+	if len(x) != len(y) {
+		return false
+	}
+	for i := range x {
+		if x[i] != y[i] {
+			return false
+		}
+	}
+	return true
+}
+
+
+// 比较map是否相等
+func equalMap(x, y map[string]int) bool {
+	if len(x) != len(y) {
+		return false
+	}
+	for k, xv := range x {
+		if yv, ok := y[k]; !ok || xv != yv {
+			return false
+		}
+	}
+	return true
+}
+
+// 字符串数组旋转
+// n 字符长度，k旋转个数， s输入切片
+func rotateSlice(n, k int, s []string) {
+	t := make([]string, k)
+	copy(t, s[len(s)-k:])
+	for i := len(s) - 1; i >= k; i-- {
+		s[i] = s[i - k]
+	}
+	copy(s[:k], t)
+}
+
+func main() {
+	a := [...]string{"1", "2", "3", "4", "5", "6", "7", "8", "9"}
+	reverse(a[:])
+	fmt.Println(a)
+
+	b := [...]string{"1", "2", "3", "4", "5", "6", "7", "8", "9"}
+	reverseArray(&b)
+	fmt.Println(b)
+
+	c := [...]string{"1", "2", "3", "4", "5", "6", "7", "8", "9"}
+	rotateSlice(9, 3, c[:])
+	fmt.Println(c)
+}
